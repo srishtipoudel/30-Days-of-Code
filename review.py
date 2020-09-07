017bc8b5c69096cdd3a0ceead64d2ca4dcd4ecbf
Srishti Poudel <srishtipoudel2385@gmail.com>
2020-09-07 13:01:57 +0545

    Day 6: Let's Review
    Task
    Given a string, S, of length N that is indexed from 0 to N-1 , print its even-indexed and odd-indexed characters as  2 space-separated strings on a single line.
    Note: 0 is considered to be an even index.
    Input Format
    The first line contains an integer, T (the number of test cases).
    Each line i of the  T subsequent lines contain a String, S .
    Constraints
      1<=T<=10
      2<=length of S<=100

---
+++ review.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+num_test_cases=int(input())
+
+for i in range(num_test_cases):
+    test_string=input()
+    even_indexed_characters= ''
+    odd_indexed_characters=''
+
+
+    for j in range(len(test_string)):
+        if j%2==0:
+            even_indexed_characters+= test_string[j]
+        else:
+            odd_indexed_characters+= test_string[j]
+
+
+    print('{} {}'.format(even_indexed_characters, odd_indexed_characters))
